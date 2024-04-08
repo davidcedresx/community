3bbb00b2ad58696a439e1c76c1745b0cdb14c5db
wilmariVielma <wilmari.vielma@gmail.com>
2024-04-08 12:00:25 -0400

    logo

--- src/pages/Students.tsx
+++ src/pages/Students.tsx
@@ -2,19 +2,19 @@ import { Stack, Table, Title } from "@mantine/core";
 
 const Students = () => {
     const elements = [
-        { position: 6, mass: 12.011, symbol: 'C', name: 'Carbon' },
-        { position: 7, mass: 14.007, symbol: 'N', name: 'Nitrogen' },
-        { position: 39, mass: 88.906, symbol: 'Y', name: 'Yttrium' },
-        { position: 56, mass: 137.33, symbol: 'Ba', name: 'Barium' },
-        { position: 58, mass: 140.12, symbol: 'Ce', name: 'Cerium' },
+        { nombre: 'wilmari', semestre: 4, telefono: '0424-7350962', cedula: '12.797.814' },
+        { nombre: 'wilmari', semestre: 4, telefono: '0424-7350962', cedula: '12.797.814' },
+        { nombre: 'wilmari', semestre: 4, telefono: '0424-7350962', cedula: '12.797.814' },
+        { nombre: 'wilmari', semestre: 5, telefono: '0424-7350962', cedula: '12.797.814' },
+        { nombre: 'wilmari', semestre: 5, telefono: '0424-7350962', cedula: '12.797.814' },
     ];
 
     const rows = elements.map((element) => (
-        <Table.Tr key={element.name}>
-            <Table.Td>{element.position}</Table.Td>
-            <Table.Td>{element.name}</Table.Td>
-            <Table.Td>{element.symbol}</Table.Td>
-            <Table.Td>{element.mass}</Table.Td>
+        <Table.Tr key={element.cedula}>
+            <Table.Td>{element.nombre}</Table.Td>
+            <Table.Td>{element.cedula}</Table.Td>
+            <Table.Td>{element.telefono}</Table.Td>
+            <Table.Td>{element.semestre}</Table.Td>
         </Table.Tr>
     ));
 
